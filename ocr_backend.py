93eb8e3c26684ec0e366edb7c890cf85a40844dd
Blu-Chips <141764134+Blu-Chips@users.noreply.github.com>
2025-06-28 02:39:13 +0300

    Update ocr_backend.py

--- ocr_backend.py
+++ ocr_backend.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from google.cloud import vision
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api/ocr', methods=['POST'])
 def ocr():
